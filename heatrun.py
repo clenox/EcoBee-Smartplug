5262bbf19f515df9ecddf7e9821436f8582be0ae
clenox <carl.lenox@gmail.com>
2018-01-13 16:32:13 -0800

    Create heatrun.py

---
+++ heatrun.py
@@ -0,0 +1,371 @@
+# Main Program
+
+# Set up libraries
+
+import sys
+import requests
+import json
+from time import sleep
+import datetime
+from pyHS100 import SmartPlug
+
+# Initialize error handling
+
+to_err = False
+
+# Read developer key from file apikey.txt, set scope
+
+a = open("apikey.txt", "r")
+APP_KEY = a.read()
+SCOPE = 'smartWrite'
+
+# Read tokens from file tokens.txt --- tokens are kept updated by token_mngr.py
+
+f = open("tokens.txt", "r")
+
+token_str = str(f.read())
+
+f.close()
+
+token_list = token_str.split(",")
+
+try:
+
+    access_token = token_list[0]
+    token_type = token_list[1]
+    refresh_token = token_list[2]
+
+except IndexError:
+
+# To do -- add token_mngr relaunch here to automate recovery from this error
+
+    sys.exit('Token File Read Error')
+
+auth = (token_type) + ' ' + (access_token)
+
+# time handling for error message
+
+timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+dectime = float(timestamp[11:13]) + float(timestamp[14:16]) / 60
+
+# Initial ecobee API call to populate room class
+
+try:
+
+    thermostat_url = 'https://api.ecobee.com/1/thermostat?'
+    payload = {'json': '{"selection":{"selectionType":"registered","includeSensors":"true"}}'}
+    header = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': auth}
+
+    response = requests.get(thermostat_url, headers=header, params=payload)
+
+    to_err = False
+
+except TimeoutError:
+
+    err_string = (str(timestamp) + ' Timeout error in ecobee data call') + "\r"
+
+    print(err_string)
+
+    log = open('log.txt', 'a+')
+    log.write(err_string)
+    log.close()
+
+    to_err = True
+
+data = response.json()
+
+# Populate lists used to build dicts
+
+namelist = []
+templist = []
+
+i = 0
+while i < len(data['thermostatList'][0]['remoteSensors']):
+    namelist.append(data['thermostatList'][0]['remoteSensors'][i]['name'])
+    templist.append(data['thermostatList'][0]['remoteSensors'][i]['capability'][0]['value'])
+    i = i + 1
+
+temps = [float(i) / 10 for i in templist]
+
+# Remove the Home sensor which is not used.  This implementation has 3 named sensors MBED, OBED and LBED
+
+namelist.remove('Home')
+
+tempdict = dict(zip(namelist, temps))
+
+print(tempdict)
+
+#  To do - replace hard coded IP addresses with file read.  Use the pyhs100 utility to find your plug ip addresses.  Replaced here with dummies.
+
+plugip_dict = {'MBED':'192.168.0.14','OBED':'192.168.0.25','LBED':'192.168.0.15'}
+
+# Assign smart plug objects
+
+mbed_plug = SmartPlug(plugip_dict['MBED'])
+obed_plug = SmartPlug(plugip_dict['OBED'])
+lbed_plug = SmartPlug(plugip_dict['LBED'])
+
+plugdict = {'MBED':mbed_plug,'LBED':lbed_plug,'OBED':obed_plug}
+
+# Define Room class
+
+class Room:
+    def __init__(self, name, temp, status, temp_high, temp_low, on_from, on_to):
+        self.name = name
+        self.temp = temp
+        self.status = status
+        self.temp_high = temp_high
+        self.temp_low = temp_low
+        self.on_from = on_from
+        self.on_to = on_to
+
+
+# Set schedule and temperature band
+# Temp setpoints in F
+# Time setpoints in decimal hours, 24 hour time
+# Note:  All time logic here assumes start after noon and end before noon!
+
+mbed = Room('MBED', tempdict['MBED'], 'OFF', 66.4, 66.2, 20.5, 7.5)
+lbed = Room('LBED', tempdict['LBED'], 'OFF', 67.0, 66.8, 19.75, 7.5)
+obed = Room('OBED', tempdict['OBED'], 'OFF', 67.0, 66.8, 19.75, 7.5)
+
+rooms = [mbed,lbed,obed]
+
+roomdict = {'MBED':mbed,'LBED':lbed,'OBED':obed}
+
+# Initialize schedule bounds -- the earliest start and latest end of schedule for all rooms
+#  Used to determine when in active vs. sleep mode overall
+
+start_time = mbed.on_from
+end_time = mbed.on_to
+
+for name in namelist:
+
+    if roomdict[name].on_from < start_time:
+
+        start_time = roomdict[name].on_from
+
+    if roomdict[name].on_to > end_time:
+
+        end_time = roomdict[name].on_to
+
+
+# Define loop time increment ... timeout between hitting APIs, 180 sec minimum per EcoBee
+
+looptime = 180
+
+# Start main loop
+
+while True:
+
+    # time handling
+
+    timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+    dectime = float(timestamp[11:13]) + float(timestamp[14:16]) / 60
+
+    while dectime >= start_time or dectime <= end_time:
+
+        timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+        dectime = float(timestamp[11:13]) + float(timestamp[14:16]) / 60
+
+        sched = True
+
+    # Get updated tokens
+    
+        f = open("tokens.txt", "r")
+
+        token_str = str(f.read())
+
+        f.close()
+
+        token_list = token_str.split(",")
+
+        try:
+
+            access_token = token_list[0]
+            token_type = token_list[1]
+            refresh_token = token_list[2]
+
+        except IndexError:
+
+    # To do -- add token_mngr relaunch here to automate recovery from this error
+
+            sys.exit('Token File Read Error')
+
+        auth = (token_type) + ' ' + (access_token)
+
+        # 4)  Call EcoBee API to get data
+
+        try:
+
+            thermostat_url = 'https://api.ecobee.com/1/thermostat?'
+            payload = {'json': '{"selection":{"selectionType":"registered","includeSensors":"true"}}'}
+            header = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': auth}
+
+            response = requests.get(thermostat_url, headers=header, params=payload)
+
+            to_err = False
+
+        except TimeoutError:
+
+            err_string = (str(timestamp) + ' Timeout error in ecobee data call') + "\r"
+
+            print(err_string)
+
+            log = open('log.txt', 'a+')
+            log.write(err_string)
+            log.close()
+
+            to_err = True
+
+            break
+
+        data = response.json()
+
+        templist = []
+
+        i = 0
+        while i < len(data['thermostatList'][0]['remoteSensors']):
+            templist.append(data['thermostatList'][0]['remoteSensors'][i]['capability'][0]['value'])
+            i = i + 1
+
+        temps = [float(i) / 10 for i in templist]
+        tempdict = dict(zip(namelist, temps))
+
+        # Assign updated temps to rooms
+
+        for name in namelist:
+
+            roomdict[name].temp = tempdict[name]
+
+            # Decision logic and set booleans for switches
+            # Plug status is updated separately from state to reduce calls to plug endpoints and allow rotation of heater operation per below
+
+            if roomdict[name].temp <= roomdict[name].temp_low and (dectime >= roomdict[name].on_from or dectime <= roomdict[name].on_to):
+
+                roomdict[name].status = "ON"
+
+            elif roomdict[name].temp > roomdict[name].temp_high or (dectime < roomdict[name].on_from and dectime > roomdict[name].on_to):
+
+                roomdict[name].status = "OFF"
+
+            else:
+
+                roomdict[name].status = roomdict[name].status
+
+        # Write log
+
+        log_str = (str(timestamp) + ", MBED, " + str(roomdict['MBED'].temp) + ", " + str(roomdict['MBED'].status) +
+            ", OBED, " + str(roomdict['OBED'].temp) + ", " + str(roomdict['OBED'].status) +
+            ", LBED, " + str(roomdict['LBED'].temp) + ", " + str(roomdict['LBED'].status) +
+            "\n")
+
+        log = open("log.txt", "a+")
+        log.write(log_str)
+        log.close()
+
+        print('Latest Timestamp: ', log_str + "\r")
+
+        # For LBED and OBED determine which will operate when both are in ON state.
+        # In this implementation these heaters are on a single breaker.
+        # This breaker trips if both are running simultaneously.
+
+        if obed.temp < lbed.temp:
+
+            lbed_turn = False
+
+        else:
+
+            lbed_turn = True
+
+        print(lbed_turn," lbed temp: ", lbed.temp," obed temp: ", obed.temp)
+
+        # Handle loss of communication with plugs
+
+        try:
+
+            mbed_plug.state = mbed.status
+            mbed_plug_err = False
+
+        except:
+
+            print('mbed plug error')
+            mbed_plug_err = True
+
+        try:
+
+            obed_plug_chk = obed_plug.state
+            obed_plug_err = False
+
+        except:
+
+            print('obed plug error')
+            obed_plug_err = True
+
+        try:
+
+            lbed_plug_chk = lbed_plug.state
+            lbed_plug_err = False
+
+        except:
+
+            print('lbed plug error')
+            lbed_plug_err = True
+
+        # Determine plug states - this logic actually turns the plugs on or off.
+
+        if obed_plug_err is False and lbed_plug_err is False:
+
+            if obed.status == 'ON' and lbed.status == 'ON' and lbed_turn is True:
+
+                obed_plug.state = 'OFF'
+                lbed_plug.state = 'ON'
+                lbed_turn = False
+                print('1 alternating obed OFF lbed ON')
+
+            elif obed.status == 'ON' and lbed.status == 'ON' and lbed_turn is False:
+
+                obed_plug.state = 'ON'
+                lbed_plug.state = 'OFF'
+                lbed_turn = True
+                print('2 alternating obed ON lbed OFF')
+
+            else:
+
+                obed_plug.state = obed.status
+                lbed_plug.state = lbed.status
+                print('3 Operating per status')
+
+        elif obed_plug_err is True and lbed_plug_err is False:
+
+            lbed_plug.state = lbed.status
+            print('4 lbed per status, obed offline')
+
+        elif lbed_plug_err is True and obed_plug_err is False:
+
+            obed_plug.state = obed.status
+            print('5 obed per status, lbed offline')
+
+        else:
+
+            print('6 obed and lbed offline')
+
+        sleep(looptime)
+
+# If outside of the schedule enter sleep mode.  No logging in sleep mode.
+
+    for name in namelist:
+
+        if(roomdict[name].status) == 'ON':
+
+            try:
+
+                plugdict[name].state = 'OFF'
+                roomdict[name].status = plugdict[name].state
+                print(roomdict[name],'turned off')
+
+            except:
+
+                print('warning, ',roomdict[name],' offline -- could not be turned off automatically')
+
+    sleep(looptime)
